4c594fb6c083aa080c1aa12bcbec08b966b342ab
npenin <npenin@dragon-angel.fr>
2018-09-28 07:04:46 +0200

    fixed clientPart to retrieve publicKey

--- src/client/api.ts
+++ src/client/api.ts
@@ -1,8 +1,16 @@
-import * as akala from '@akala/server';
+import * as akala from '@akala/core';
 import { SerializableObject } from '@akala/json-rpc-ws';
 
 export var api = new akala.Api()
     .clientToServerOneWay<{ name: string, user: string }>()({ register: true })
     .serverToClientOneWay<{ user: string, notification: SerializableObject }>()({ notify: true })
     .clientToServerOneWay<{ name: string, user: string, notification: SerializableObject }>()({ notify: true })
+    .clientToServer<void, string>()({
+        getPublicKey: {
+            jsonrpcws: true, rest: {
+                url: '/publicKey',
+                method: 'get'
+            }
+        }
+    })
     ;
--- src/client/routes.ts
+++ src/client/routes.ts
@@ -5,42 +5,48 @@ declare var swRegistration: PromiseLike<ServiceWorkerRegistration>;
 
 if (Notification.permission !== 'denied')
 {
-    const publicKey = 'BDU5tQ-WzGDUJOsE3743I9LeyfolOYQH66vux5boQxFHj-d_rJkaLmFje6h-Iuv9AwnrgfpF4jkLbY3LTYpWjZQ';
-    function urlB64ToUint8Array(base64String)
+    akala.injectWithName(['$http'], async function (http: akala.Http)
     {
-        const padding = '='.repeat((4 - base64String.length % 4) % 4);
-        const base64 = (base64String + padding)
-            .replace(/\-/g, '+')
-            .replace(/_/g, '/');
 
-        const rawData = window.atob(base64);
-        const outputArray = new Uint8Array(rawData.length);
-
-        for (let i = 0; i < rawData.length; ++i)
+        // const publicKey = 'BDU5tQ-WzGDUJOsE3743I9LeyfolOYQH66vux5boQxFHj-d_rJkaLmFje6h-Iuv9AwnrgfpF4jkLbY3LTYpWjZQ';
+        function urlB64ToUint8Array(base64String)
         {
-            outputArray[i] = rawData.charCodeAt(i);
+            const padding = '='.repeat((4 - base64String.length % 4) % 4);
+            const base64 = (base64String + padding)
+                .replace(/\-/g, '+')
+                .replace(/_/g, '/');
+
+            const rawData = window.atob(base64);
+            const outputArray = new Uint8Array(rawData.length);
+
+            for (let i = 0; i < rawData.length; ++i)
+            {
+                outputArray[i] = rawData.charCodeAt(i);
+            }
+            return outputArray;
         }
-        return outputArray;
-    }
 
-    swRegistration.then(reg =>
-    {
-        const applicationServerKey = urlB64ToUint8Array(publicKey);
+        var notificationClient = akala.api.rest(api).createServerProxy('/api/@domojs/notifications')
 
-        reg.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: applicationServerKey
-        })
-            .then(function (subscription)
-            {
-                console.log('User is subscribed.');
+        Promise.all([swRegistration, notificationClient.getPublicKey(void)]).then(([reg, publicKey]) =>
+        {
+            const applicationServerKey = urlB64ToUint8Array(publicKey);
 
+            reg.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: applicationServerKey
             })
-            .catch(function (err)
-            {
-                console.log('Failed to subscribe the user: ', err);
-            });
-    });
+                .then(function (subscription)
+                {
+                    console.log('User is subscribed.');
+
+                })
+                .catch(function (err)
+                {
+                    console.log('Failed to subscribe the user: ', err);
+                });
+        });
+    })();
 }
 
 akala.injectWithName(['$agent.@domojs/notifications'], function (agent)
